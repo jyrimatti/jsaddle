acd1aa05f1cd9cba8a0cf75e21b0bf837323ba4f
Athan Clark <athan.clark@gmail.com>
2021-07-31 08:25:52 -0400

    fixing #64

--- jsaddle-warp/src/Language/Javascript/JSaddle/WebSockets.hs
+++ jsaddle-warp/src/Language/Javascript/JSaddle/WebSockets.hs
@@ -225,12 +225,13 @@ jsaddleJs' jsaddleUri refreshOnLoad = "\
     \            }\n\
     \\n\
     \ " <> runBatch (\a -> "ws.send(JSON.stringify(" <> a <> "));")
-              (Just (\a -> "(function(){\n\
-                  \                       var xhr = new XMLHttpRequest();\n\
-                  \                       xhr.open('POST', '" <> fromMaybe "" jsaddleUri <> "/sync/'+syncKey, false);\n\
-                  \                       xhr.setRequestHeader(\"Content-type\", \"application/json\");\n\
-                  \                       xhr.send(JSON.stringify(" <> a <> "));\n\
-                  \                       return JSON.parse(xhr.response);})()")) <> "\
+--              (Just (\a -> "(function(){\n\
+--                  \                       var xhr = new XMLHttpRequest();\n\
+--                  \                       xhr.open('POST', '" <> fromMaybe "" jsaddleUri <> "/sync/'+syncKey, false);\n\
+--                  \                       xhr.setRequestHeader(\"Content-type\", \"application/json\");\n\
+--                  \                       xhr.send(JSON.stringify(" <> a <> "));\n\
+--                  \                       return JSON.parse(xhr.response);})()")) <> "\
+              Nothing <> "\
     \        };\n\
     \    };\n\
     \    ws0.onerror = function() {\n\
