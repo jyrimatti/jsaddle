5cc5efa126bf73389dcfbe0d0a4492ada8e75b07
Athan Clark <athan.clark@gmail.com>
2021-08-01 08:58:19 -0400

    runWithIndex

--- jsaddle-warp/src/Language/Javascript/JSaddle/Warp.hs
+++ jsaddle-warp/src/Language/Javascript/JSaddle/Warp.hs
@@ -15,7 +15,7 @@
 
 module Language.Javascript.JSaddle.Warp (
   -- * Running JSM over WebSockets
-    run
+    run, runWithIndex
 #ifndef ghcjs_HOST_OS
   , module Language.Javascript.JSaddle.WebSockets
 #endif
@@ -30,6 +30,7 @@ import Language.Javascript.JSaddle.Types (JSM)
 import Language.Javascript.JSaddle.Run (syncPoint)
 import Language.Javascript.JSaddle.WebSockets
 #endif
+import Data.ByteString.Lazy (ByteString)
 
 -- | Run the given 'JSM' action as the main entry point.  Either directly
 --   in GHCJS or as a Warp server on the given port on GHC.
@@ -41,4 +42,17 @@ run :: Int -> JSM () -> IO ()
 run port f =
     runSettings (setPort port (setTimeout 3600 defaultSettings)) =<<
         jsaddleOr defaultConnectionOptions (f >> syncPoint) jsaddleApp
+#endif
+
+
+-- | Run the given 'JSM' action as the main entry point.  Either directly
+--   in GHCJS or as a Warp server on the given port on GHC.
+#ifdef ghcjs_HOST_OS
+runWithIndex :: ByteString -> Int -> IO () -> IO ()
+runWithIndex _idx _port = id
+#else
+runWithIndex :: ByteString -> Int -> JSM () -> IO ()
+runWithIndex idx port f =
+    runSettings (setPort port (setTimeout 3600 defaultSettings)) =<<
+        jsaddleOrWithIndex idx defaultConnectionOptions (f >> syncPoint) jsaddleApp
 #endif
